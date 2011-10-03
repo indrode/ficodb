266dee4388c9bcaffb5bf41ec1f84b6319b94b84
Indro De <indro.de@gmail.com>
2011-10-03 17:00:24 +0200

    create new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ficodb::Application.config.secret_token = '5fc4e1d0bf555f1f8c5c71744eb1611d560d0c70e992f65098bc1f908b36458c0b49166c3a71207028a335052465203e1c72d8f2205c73bb0e951cc1606083b8'
