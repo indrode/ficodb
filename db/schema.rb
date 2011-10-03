b86607d70b9eaaad4d375dd6ab11aa5ea028304c
Indro De <indro.de@gmail.com>
2011-10-04 01:45:37 +0200

    add feature model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20111003234205) do
+
+  create_table "features", :force => true do |t|
+    t.string   "title"
+    t.boolean  "enabled"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
